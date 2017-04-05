faa20bd7693622de6bf4dd05bf806f4ece83080d
Eitan Suez <eitan.suez@gmail.com>
2017-04-04 20:45:38 -0500

    whitespace/formatting

--- greeting-ribbon/src/main/java/io/pivotal/GreetingRibbonApplication.java
+++ greeting-ribbon/src/main/java/io/pivotal/GreetingRibbonApplication.java
@@ -8,9 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class GreetingRibbonApplication {
 
-	
-    public static void main(String[] args) {
-        SpringApplication.run(GreetingRibbonApplication.class, args);
-    }
- 
+  public static void main(String[] args) {
+    SpringApplication.run(GreetingRibbonApplication.class, args);
+  }
+
 }
