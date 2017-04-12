942bc4666147225eb0aa1b741ae21b5a495ef0e7
Eitan Suez <eitan.suez@gmail.com>
2017-04-12 15:53:08 -0600

    refactored implementation of fortuneservice; also added the ability to configure a delay/latency in responding via property DELAY_MS

--- fortune-service/src/main/java/io/pivotal/fortune/FortuneController.java
+++ fortune-service/src/main/java/io/pivotal/fortune/FortuneController.java
@@ -3,22 +3,38 @@ package io.pivotal.fortune;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FortuneController {
-  Logger logger = LoggerFactory
-      .getLogger(FortuneController.class);
+
+  Logger logger = LoggerFactory.getLogger(FortuneController.class);
+
   @Autowired
   private FortuneService fortuneService;
 
+  @Value("${delay.ms:0}")
+  private int delayMs = 0;
 
   @RequestMapping("/")
   String getQuote() {
     logger.debug("fetching fortune.");
+
+    artificialDelay();
+
     return fortuneService.getFortune();
   }
 
+  private void artificialDelay() {
+    if (delayMs <= 0) return;
+
+    try {
+      Thread.sleep(delayMs);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
