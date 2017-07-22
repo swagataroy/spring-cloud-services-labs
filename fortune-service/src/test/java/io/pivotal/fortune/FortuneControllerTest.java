8094182f7770020372322663a64d89b17e9a9012
Eitan Suez <eitan.suez@gmail.com>
2017-07-22 10:39:39 -0500

    application of spring rest docs to fortune service, just to familiarize myself with it

--- fortune-service/src/test/java/io/pivotal/fortune/FortuneControllerTest.java
+++ fortune-service/src/test/java/io/pivotal/fortune/FortuneControllerTest.java
@@ -1,28 +1,33 @@
 package io.pivotal.fortune;
 
-import io.pivotal.FortuneServiceApplication;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = FortuneServiceApplication.class)
+@SpringBootTest(webEnvironment = MOCK)
 public class FortuneControllerTest {
+
+  @Rule
+  public JUnitRestDocumentation restDocumentation =
+      new JUnitRestDocumentation("target/generated-snippets");
 
   @Autowired
   private FortuneController fortuneController;
@@ -37,15 +42,19 @@ public class FortuneControllerTest {
   @Before
   public void setup() {
     when(fortuneService.getFortune()).thenReturn(fortune);
-    this.mockMvc = MockMvcBuilders.standaloneSetup(fortuneController).build();
+    this.mockMvc = MockMvcBuilders
+        .standaloneSetup(fortuneController)
+        .apply(documentationConfiguration(restDocumentation))
+        .build();
   }
 
   @Test
   public void getFortune() throws Exception {
     this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
         .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json;charset=UTF-8"))
-        .andExpect(content().string(fortune));
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(content().string(fortune))
+        .andDo(document("index"));
   }
 
 }
