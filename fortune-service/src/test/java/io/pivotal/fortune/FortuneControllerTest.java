1627eb5baba9f19b73182aaab2d8d38752fcf2e0
Eitan Suez <eitan.suez@gmail.com>
2018-01-25 13:04:53 -0600

    upgraded projects from boot 1.5.2 to 1.5.9 and from Dalston to Edgware Release

--- fortune-service/src/test/java/io/pivotal/fortune/FortuneControllerTest.java
+++ fortune-service/src/test/java/io/pivotal/fortune/FortuneControllerTest.java
@@ -1,22 +1,18 @@
 package io.pivotal.fortune;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,10 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = MOCK)
 public class FortuneControllerTest {
-
-  @Rule
-  public JUnitRestDocumentation restDocumentation =
-      new JUnitRestDocumentation("target/generated-snippets");
 
   @Autowired
   private FortuneController fortuneController;
@@ -44,7 +36,6 @@ public class FortuneControllerTest {
     when(fortuneService.getFortune()).thenReturn(fortune);
     this.mockMvc = MockMvcBuilders
         .standaloneSetup(fortuneController)
-        .apply(documentationConfiguration(restDocumentation))
         .build();
   }
 
@@ -53,8 +44,7 @@ public class FortuneControllerTest {
     this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-        .andExpect(content().string(fortune))
-        .andDo(document("index"));
+        .andExpect(content().string(fortune));
   }
 
 }
